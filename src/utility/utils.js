9f6060ad1ebb2429851e122b58a33a6b9459ce11
John Hamm <johndhamm1@gmail.com>
2019-03-13 14:33:00 -0500

    style display of times in WorkoutDetail

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -1,9 +1,17 @@
 import _ from 'lodash'
+import TimeConversion from './time_conversion'
 
 const Utils = {
   createTeamList: (teamStore) => {
     return _.sortBy(_.map(teamStore), 'name');
   },
+  createDisplayTime: (timeMS) => {
+    let timeObj = {};
+    const time =TimeConversion(timeMS).split('.');
+    timeObj.main = time[0];
+    timeObj.decimal = time[1];
+    return timeObj;
+  }
 
 };
 
